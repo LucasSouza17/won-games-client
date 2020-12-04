6b97d973916299749730543b560983782c00930b
Lucas Fernando de Souza <62787018+LucasSouza17@users.noreply.github.com>
2020-12-04 13:55:10 -0300

    Banner component

---
+++ src/components/Banner/stories.tsx
@@ -0,0 +1,20 @@
+import { Story, Meta } from '@storybook/react/types-6-0'
+import Banner, { BannerProps } from '.'
+
+export default {
+  title: 'Banner',
+  component: Banner,
+  args: {
+    img:
+      'https://images.unsplash.com/photo-1582552281350-20f4bc81566d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1086&q=80',
+    title: 'Defy death',
+    subtitle: '<p>Play the new <strong>CrashLands</strong> season',
+    buttonLabel: 'Buy now',
+    buttonLink: '/games/defy-death'
+  },
+  parameters: {
+    layout: 'fullscreen'
+  }
+} as Meta
+
+export const Default: Story<BannerProps> = (args) => <Banner {...args} />
