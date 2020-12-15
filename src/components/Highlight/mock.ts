a0c9008375aee9c07dff1a0b99f5a8cf683cfadf
Lucas Fernando de Souza <62787018+LucasSouza17@users.noreply.github.com>
2020-12-14 22:03:08 -0300

    page home created

---
+++ src/components/Highlight/mock.ts
@@ -0,0 +1,7 @@
+export default {
+  title: "Red Dead it's back",
+  subtitle: "Came see John's new adventures",
+  buttonLabel: 'Buy now',
+  buttonLink: '/rdr2',
+  backgroundImage: '/img/red-dead-img.jpg'
+}
