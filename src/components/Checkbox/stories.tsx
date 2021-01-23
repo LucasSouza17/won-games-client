2740350cd686533af5fed550d4d711a93d7abe28
Lucas Fernando de Souza <62787018+LucasSouza17@users.noreply.github.com>
2021-01-22 21:59:56 -0300

    checkbox component

---
+++ src/components/Checkbox/stories.tsx
@@ -0,0 +1,46 @@
+import { Story, Meta } from '@storybook/react/types-6-0'
+import Checkbox, { CheckboxProps } from '.'
+
+export default {
+  title: 'Checkbox',
+  component: Checkbox,
+  parameters: {
+    layout: 'fullscreen',
+    backgrounds: {
+      default: 'won-dark'
+    }
+  },
+  argTypes: {
+    onCheck: { action: 'checked' }
+  }
+} as Meta
+
+export const Default: Story<CheckboxProps> = (args) => (
+  <>
+    <div style={{ padding: 10 }}>
+      <Checkbox
+        isChecked
+        {...args}
+        name="category"
+        label="Action"
+        labelFor="action"
+      />
+    </div>
+    <div style={{ padding: 10 }}>
+      <Checkbox
+        {...args}
+        name="category"
+        label="Adventure"
+        labelFor="adventure"
+      />
+    </div>
+    <div style={{ padding: 10 }}>
+      <Checkbox
+        {...args}
+        name="category"
+        label="Strategy"
+        labelFor="strategy"
+      />
+    </div>
+  </>
+)
